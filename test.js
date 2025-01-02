a0659f13d241449c3f6d826ae8c96618f37e48ea
Siddique Chengapra <siddiquechengapra@gmail.com>
2025-01-02 18:14:12 +0530

    simple closure example

--- test.js
+++ test.js
@@ -1,28 +1,13 @@
-
- function ListNode(val, next) {
-     this.val = (val===undefined ? 0 : val)
-     this.next = (next===undefined ? null : next)
-  }
-
- 
-var addTwoNumbers = function(l1, l2) {
-    let ans =[]
-    let ans2 =[]
- 
-    let start=l1
-    while(start){
-     ans.push(start.val)
-     start=start.next
+let parent=()=>{
+    let counter = 0
+    return  child=()=>{
+        counter++
+        console.log("counter is ",counter)
     }
-    let start2=l2
-    while(start2){
-     ans2.push(start2.val)
-     start2=start2.next
-    }
-    ans.reverse()
-    ans2.reverse()
-    
-     console.log("vl1,val2",ans,ans2)
- };
+}
 
- addTwoNumbers([2,4,3],[5,6,4])
+let res= parent()
+res()
+res()
+res()
+
