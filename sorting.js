77969d717cc9a16893e3b428128d0293d47cb724
Sid <Sid@users-MacBook-Pro.local>
2026-08-31 02:25:43 +0530

    revised sorting algorithms

--- sorting.js
+++ sorting.js
@@ -41,23 +41,63 @@
 //     }
 //     return -1
 // }
-let nums = [3,4,5,7,1,2]
+// let nums = [3,4,5,7,1,2]
+//         [ 1,3,4,5,7,2]
+//           p
+// function insertionSort(nums){
+//     for(let i=1;i<nums.length;i++){
+//         let curr=nums[i]
+//         let prev= i-1
+//         while(curr<nums[prev]&& prev>=0){
+//             nums[prev+1]=nums[prev]
+//             prev--
+//         }
+//         nums[prev+1]=curr
+//     }
+//     return nums
+// }
 
-function insertionSort(nums){
-    for(let i=1;i<nums.length;i++){
-        let curr=nums[i]
-        let prev= i-1
-        while(curr<nums[prev]&& prev>=0){
-            nums[prev+1]=nums[prev]
-            prev--
-        }
-        nums[prev+1]=curr
-    }
-    return nums
-}
+let arr = [
+    42, 17, 89, 3, 56, 21, 74, 8,
+    35, 91, 12, 67, 29, 50, 6, 83,
+    14, 72, 45, 1, 63, 27, 98, 10,
+    38, 55, 19, 81, 4, 69, 31, 87,
+    23, 60, 11, 76, 48, 2, 94, 33
+];
+
+// function mergeSort(arr) {
+//     let middle = Math.floor(arr.length / 2)
+//     if (arr.length === 1) {
+//         return arr
+//     }
+//     let left = mergeSort(arr.slice(0, middle))
+//     let right = mergeSort(arr.slice(middle))
+
+//     return merge(left, right)
+
+// }
+// function merge(arr1, arr2) {
+//     let m = arr1.length 
+//     let n = arr2.length 
+//     let p1 = m-1
+//     let p2 = n-1
+
+//     for (let i = m + n - 1; i >= 0; i--) {
+//         if (p2 < 0) {
+//             break
+//         }
+//         if ( p1 >= 0 && arr1[p1] > arr2[p2] ) {
+//             arr1[i] = arr1[p1]
+//             p1--
+//         } else {
+//             arr1[i] = arr2[p2]
+//             p2--
+//         }
+//     }
+
+//     return arr1
+// }
+// --------------------------------------------------------------------------
 
 
-
-
-
-console.log(insertionSort(nums))
+console.log(insertionSort(arr))
